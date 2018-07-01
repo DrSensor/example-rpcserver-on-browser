91524675ec62a8b2572af0f6ed7865dcbc9744df
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-07-01 21:40:58 +0700

    ✨ install and use svelte-loader

--- src/webpack.config.ts
+++ src/webpack.config.ts
@@ -5,6 +5,15 @@ import HtmlWebpackPlugin = require('html-webpack-plugin')
 export default {
   mode: 'development',
   entry: path.resolve(__dirname, '../public/index.js'),
+  module: {
+    rules: [{
+      test: /\.html$/,
+      use: [{
+        loader: 'svelte-loader',
+        options: {hotReload: true}
+      }]
+    }]
+  },
   plugins: [
     new HtmlWebpackPlugin()
   ]
